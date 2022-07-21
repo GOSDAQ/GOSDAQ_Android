9fd7226787eadf50ce263f9efddfbba66fcecbf9
Seongbo <hsb9504@gmail.com>
2022-07-21 22:54:24 +0900

    API 요청 실패 시 재시도 로직 추가

--- app/src/main/java/com/project/gosdaq/repository/remote/GosdaqServiceDataSource.kt
+++ app/src/main/java/com/project/gosdaq/repository/remote/GosdaqServiceDataSource.kt
@@ -1,5 +1,6 @@
 package com.project.gosdaq.repository.remote
 
+import android.util.Log
 import com.project.gosdaq.BuildConfig
 import com.project.gosdaq.dao.InterestingResponse.InterestingResponseDao
 import retrofit2.Call
@@ -9,6 +10,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class GosdaqServiceDataSource : GosdaqServiceDataSourceImpl {
+    private val TAG = this.javaClass.simpleName
+
     private val gosdaqBuilder = Retrofit.Builder()
         .baseUrl(BuildConfig.API_URL)
         .addConverterFactory(GsonConverterFactory.create())
@@ -27,15 +30,24 @@ class GosdaqServiceDataSource : GosdaqServiceDataSourceImpl {
 
         gosdaqService.getInterestingData(payload)
             .enqueue(object : Callback<InterestingResponseDao> {
+                var onFailureRetryCount = 0
+
                 override fun onResponse(
                     call: Call<InterestingResponseDao>,
                     response: Response<InterestingResponseDao>
                 ) {
+                    Log.i(TAG, "onResponse")
                     stockDataCallback.onResponse(response.body()!!)
                 }
 
                 override fun onFailure(call: Call<InterestingResponseDao>, t: Throwable) {
-                    stockDataCallback.onFailure(t)
+                    onFailureRetryCount += 1
+                    if (onFailureRetryCount < 2) {
+                        Log.i(TAG, "onFailure, $onFailureRetryCount times retry")
+                        call.clone().enqueue(this)
+                    } else {
+                        stockDataCallback.onFailure(t)
+                    }
                 }
             })
     }
