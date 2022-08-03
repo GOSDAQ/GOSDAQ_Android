d1d24642fd866c1a6fe762437fe26b0d80a492e4
Seongbo <hsb9504@gmail.com>
2022-08-03 23:04:56 +0900

    장시간 heroku와 통신을 진행하지 않았을 때 Timeout 문제 수정
    * API 요청 실패 시 재시도 로직 추가
    
    * API 요청 실패에 대한 재시도 로직 함수화

--- app/src/main/java/com/project/gosdaq/repository/remote/GosdaqServiceDataSource.kt
+++ app/src/main/java/com/project/gosdaq/repository/remote/GosdaqServiceDataSource.kt
@@ -1,5 +1,6 @@
 package com.project.gosdaq.repository.remote
 
+import android.util.Log
 import com.project.gosdaq.BuildConfig
 import com.project.gosdaq.dao.InterestingResponse.InterestingResponseDao
 import retrofit2.Call
@@ -9,6 +10,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class GosdaqServiceDataSource : GosdaqServiceDataSourceImpl {
+    private val TAG = this.javaClass.simpleName
+
     private val gosdaqBuilder = Retrofit.Builder()
         .baseUrl(BuildConfig.API_URL)
         .addConverterFactory(GsonConverterFactory.create())
@@ -27,6 +30,8 @@ class GosdaqServiceDataSource : GosdaqServiceDataSourceImpl {
 
         gosdaqService.getInterestingData(payload)
             .enqueue(object : Callback<InterestingResponseDao> {
+                var onFailureRetryCount = 0
+
                 override fun onResponse(
                     call: Call<InterestingResponseDao>,
                     response: Response<InterestingResponseDao>
@@ -35,7 +40,17 @@ class GosdaqServiceDataSource : GosdaqServiceDataSourceImpl {
                 }
 
                 override fun onFailure(call: Call<InterestingResponseDao>, t: Throwable) {
-                    stockDataCallback.onFailure(t)
+                    onFailureRetryCount += 1
+                    if (onFailureRetryCount < 3) {
+                        retryOnFailure(call)
+                    } else {
+                        stockDataCallback.onFailure(t)
+                    }
+                }
+
+                fun retryOnFailure(call: Call<InterestingResponseDao>) {
+                    Log.i(TAG, "onFailure, retry $onFailureRetryCount times")
+                    call.clone().enqueue(this)
                 }
             })
     }
